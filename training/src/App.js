6deb04ee0fae5a0e20c11dd0900bdb2ed7ce3a01
leen <leenodeh287@gmail.com>
2023-09-20 13:41:43 +0300

    test class-component

--- training/src/App.js
+++ training/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Greet from './Component/Greet/Greet'
+import Welcome from './Component/Welcom/Welcome'
 
 const App = () => {
   return (
     <div>
       <Greet/>
+      <Welcome/>
     </div>
   )
 }
