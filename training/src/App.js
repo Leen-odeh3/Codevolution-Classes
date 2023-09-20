44ae189a0fa5bbd10509c3bf663c1851d001c947
leen <leenodeh287@gmail.com>
2023-09-20 15:05:30 +0300

    Conditional Rendering

--- training/src/App.js
+++ training/src/App.js
@@ -1,19 +1,29 @@
 import React from 'react'
-import Greet from './Component/Greet/Greet'
-import Welcome from './Component/Welcom/Welcome'
-import Counter from './Component/Counter/Counter'
-import Destructuring from './Component/Destructuring/Destructuring'
+// import Greet from './Component/Greet/Greet'
+// import Welcome from './Component/Welcom/Welcome'
+// import Counter from './Component/Counter/Counter'
+// import Destructuring from './Component/Destructuring/Destructuring'
+import User from './Component/User/User.jsx'
 
 const App = () => {
   return (
+    <>
     <div>
-      <Greet name="Leen"/>
+      {/* <Greet name="Leen"/>
       <Greet name="kareem"/>
       <Greet name="Marah"/>
       <Welcome/>
       <Counter/> 
-      <Destructuring name="leen" age="22" address="tulkarm"/>
+      <Destructuring name="leen" age="22" address="tulkarm"/> */}
+<User/>
+
     </div>
+    <div>
+
+
+
+    </div>
+    </>
   )
 }
 
---
+++ training/src/Component/User/User.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const User = () => {
+    const name="leen"
+  return (
+    <div>
+      {name === 'kareem' ? "hello ...": "Hello leen"}
+    </div>
+  )
+}
+
+export default User
