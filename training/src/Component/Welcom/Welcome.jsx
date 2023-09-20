801a994c52ad0c622c4b529953fb2d8da0eedf0b
leen <leenodeh287@gmail.com>
2023-09-20 14:23:18 +0300

    setstate class-component

--- training/src/Component/Welcom/Welcome.jsx
+++ training/src/Component/Welcom/Welcome.jsx
@@ -1,11 +1,27 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class Welcome extends Component {
+  state = {
+    message: "Hello visitors!..",
+  };
+
+  SharedWorker() {
+    this.setState({ message: "Thank you for subscribing!" });
+  }
+
   render() {
     return (
       <div>
-        <h1>Welcome..</h1>
+        {/* <h1>Welcome..</h1> */}
+        <p>{this.state.message}</p>
+        <button
+          onClick={() => {
+            this.SharedWorker();
+          }}
+        >
+          Subscribe
+        </button>
       </div>
-    )
+    );
   }
 }
