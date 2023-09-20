f9ac34e159b6b80c5692f6b1ad950e90414dcb3f
leen <leenodeh287@gmail.com>
2023-09-20 15:09:00 +0300

    List Rendering

--- training/src/App.js
+++ training/src/App.js
@@ -1,30 +1,27 @@
-import React from 'react'
-// import Greet from './Component/Greet/Greet'
-// import Welcome from './Component/Welcom/Welcome'
-// import Counter from './Component/Counter/Counter'
-// import Destructuring from './Component/Destructuring/Destructuring'
-import User from './Component/User/User.jsx'
+import React from "react";
+import Greet from "./Component/Greet/Greet";
+import Welcome from "./Component/Welcom/Welcome";
+import Counter from "./Component/Counter/Counter";
+import Destructuring from "./Component/Destructuring/Destructuring";
+import User from "./Component/User/User.jsx";
+import Numbers from "./Component/Numbers/Numbers";
 
 const App = () => {
   return (
     <>
-    <div>
-      {/* <Greet name="Leen"/>
-      <Greet name="kareem"/>
-      <Greet name="Marah"/>
-      <Welcome/>
-      <Counter/> 
-      <Destructuring name="leen" age="22" address="tulkarm"/> */}
-<User/>
-
-    </div>
-    <div>
-
-
-
-    </div>
+      <div>
+        <Greet name="Leen" />
+        <Greet name="kareem" />
+        <Greet name="Marah" />
+        <Welcome />
+        <Counter />
+        <Destructuring name="leen" age="22" address="tulkarm" />
+        <User />
+      <Numbers/>
+      </div>
+      <div></div>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
